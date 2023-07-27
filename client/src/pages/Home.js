9a8bcc9c64ecc6c12566f5106dcffa949321c9cb
Tanmay Gupta <tanmaygupta2021@gmail.com>
2023-07-27 13:25:07 +0530

    removed login to try omdb api

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,27 +10,27 @@ function Home () {
     const navigate = useNavigate();
     const [details, setDetails] = useState('');
 
-    useEffect(() => {
-        const token = sessionStorage.getItem('token');
+    // useEffect(() => {
+    //     const token = sessionStorage.getItem('token');
 
-        if(!token) {
-            navigate('/login')
-        }
+    //     if(!token) {
+    //         navigate('/login')
+    //     }
 
-        if(token) {
-            const user = jwt_decode(token);
+    //     if(token) {
+    //         const user = jwt_decode(token);
 
-            if(!user) {
-                sessionStorage.removeItem('token');
-                navigate('/login');
-            }
-            else{
-                console.log(user);
-                setDetails(user);
-            }
-        }
-        // eslint-disable-next-line
-    }, []);
+    //         if(!user) {
+    //             sessionStorage.removeItem('token');
+    //             navigate('/login');
+    //         }
+    //         else{
+    //             console.log(user);
+    //             setDetails(user);
+    //         }
+    //     }
+    //     // eslint-disable-next-line
+    // }, []);
 
     <Routes>
         <Route path="/profile" element={<Profile />}/>
